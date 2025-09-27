8a44d631cc6d3a5c0ca92bd8636d84f2b5ed3284
emcentee94 <emcentee@protonmail.com>
2025-09-27 13:16:58 +1000

    chore: Enhance ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
 
 
 export default [
@@ -15,6 +18,19 @@ export default [
         ...pluginReactConfig.rules,
         "react/react-in-jsx-scope": "off",
         "react/jsx-no-target-blank": "off",
+    }
+  },
+  {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+      "import": importPlugin,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "jsx-a11y/alt-text": "warn",
+      "import/order": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     }
   }
 ];

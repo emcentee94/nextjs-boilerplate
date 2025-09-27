60b3cbd616ac9d3cf01b561ffc0b7cfd484398f1
emcentee94 <emcentee@protonmail.com>
2025-09-27 11:44:46 +1000

    chore: Optimize project workflow and apply formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 // eslint.config.js
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+
 export default [
-  {
-    rules: {
-      semi: 'error',
-      'prefer-const': 'error',
-    },
-  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
 ]

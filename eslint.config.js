5f2162536f8dcbc8bc2e3ae3d90a024005fba3e1
emcentee94 <emcentee@protonmail.com>
2025-09-27 13:15:36 +1000

    chore: Update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
 // eslint.config.js
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
-]
+  {
+    ...pluginReactConfig,
+    rules: {
+        ...pluginReactConfig.rules,
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-no-target-blank": "off",
+    }
+  }
+];
